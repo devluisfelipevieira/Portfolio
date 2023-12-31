03d92c9f4de45e1852c44bbed96df019b4b16548
Luis Felipe <114487959+luisnascimentovieira@users.noreply.github.com>
2023-12-31 11:45:40 -0300

    separando a área dos Cards em Components

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Card, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import styles from "./styles.module.scss";
-import Link from "next/link";
+import Cards from "@/components/Cards";
 
 export default function Projects() {
   return (
@@ -15,54 +13,8 @@ export default function Projects() {
           programação JavaScript/TypeScript <br /> E todos os códigos estão
           documentados no meu portifólio do GitHub
         </p>
-        <Container className={styles.cardsContainer}>
-          <Card style={{ width: "30rem" }} className={styles.card}>
-            <Card.Img variant="top" src="/images/portifolio-card.jpg" />
-            <Card.Body>
-              <Card.Title>Portifolio</Card.Title>
-              <Card.Text>
-                <p>
-                  Este portifolio foi criado com o intuito de demonstrar meus
-                  projetos de forma simples e direto ao ponto.
-                </p>
-                <p>
-                  Está página é uma SPA totalmente responsiva e é capaz de se
-                  adaptar à tela de qualquer dispisitivo
-                </p>
-                Ferramentas usadas no projeto: <br />
-                <a
-                  target="_blank"
-                  href="https://nextjs.org/"
-                  className={styles.tecsOnPage}
-                >
-                  Next.js
-                </a>
-                <br />
-                <a
-                  target="_blank"
-                  href="https://getbootstrap.com/"
-                  className={styles.tecsOnPage}
-                >
-                  Bootstrap
-                </a>
-                <br />
-                <a
-                  target="_blank"
-                  href="https://sass-lang.com/"
-                  className={styles.tecsOnPage}
-                >
-                  Sass
-                </a>
-              </Card.Text>
-              <Link href={"/"}>
-                <Button className={styles.cardButton} variant="dark">
-                  Ir para Home da Página
-                </Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </Container>
       </Container>
+      <Cards />
     </>
   );
 }
---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card, Container } from "react-bootstrap";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+export default function Cards() {
+  return (
+    <Container className={styles.cardsContainer}>
+      <Card style={{ width: "30rem" }} className={styles.card}>
+        <Card.Img variant="top" src="/images/portifolio-card.jpg" />
+        <Card.Body>
+          <Card.Title>Portifólio</Card.Title>
+          <Card.Text>
+            <p>
+              Este portifólio foi criado com o intuito de demonstrar meus
+              projetos de forma simples e direto ao ponto.
+            </p>
+            <p>
+              Está página é uma SPA totalmente responsiva e é capaz de se
+              adaptar à tela de qualquer dispisitivo
+            </p>
+            Ferramentas usadas no projeto: <br />
+            <a
+              target="_blank"
+              href="https://nextjs.org/"
+              className={styles.tecsOnPage}
+            >
+              Next.js
+            </a>
+            <br />
+            <a
+              target="_blank"
+              href="https://getbootstrap.com/"
+              className={styles.tecsOnPage}
+            >
+              Bootstrap
+            </a>
+            <br />
+            <a
+              target="_blank"
+              href="https://sass-lang.com/"
+              className={styles.tecsOnPage}
+            >
+              Sass
+            </a>
+          </Card.Text>
+          <Link href={"/"}>
+            <Button className={styles.cardButton} variant="dark">
+              Ir para Home da Página
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
